cd083187e21dee4547c4bd39f6d738852af34948
unknown <asederado@gmail.com>
2023-02-14 23:29:11 +0100

    add base url

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import svgrPlugin from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), viteTsconfigPaths(), svgrPlugin(), mkcert()],
+    base: '/gox.earth/',
     build: {
         outDir: "build",
     }
